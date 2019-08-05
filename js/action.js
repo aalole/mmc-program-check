1bd4533fd84a68b56dd42fdc376c7fbd3bb0b415
aalole <abiodunrasheed93@gmail.com>
2019-08-05 14:55:04 +0200

    mmc websit laid out completely

---
+++ js/action.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("button");
+button.addEventListener("click", e=>{
+  window.location.assign("file:///C:/Users/AALOLE25/Desktop/MMC-Website/champions.html");
+})
